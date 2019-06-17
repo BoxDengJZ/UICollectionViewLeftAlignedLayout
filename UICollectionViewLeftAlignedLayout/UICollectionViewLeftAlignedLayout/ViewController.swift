152b75a6940da1b5add2b662385986f68ada7cf5
邓江洲 <707703386@qq.com>
2019-06-17 13:04:43 +0800

    initial commit

---
+++ UICollectionViewLeftAlignedLayout/UICollectionViewLeftAlignedLayout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UICollectionViewLeftAlignedLayout
+//
+//  Created by Jz D on 2019/6/17.
+//  Copyright © 2019 Jz D. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
